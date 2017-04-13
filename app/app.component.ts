bc8316e047c318357feb6485e8bb497bdb0bbd5c
Jeff Smyth <jeffrey.smyth@synchronoss.com>
2017-04-12 21:55:17 -0400

    Inject the ElementRef and use it in the class as an example

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>AppComponent template!</h1>'
+  template: `<h1 id="header">AppComponent template</h1>
+  <input (click)="addIt()" type="button" value="Add text">
+  <input (click)="removeIt()" type="button" value="Remove text">
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(protected element: ElementRef) { }
+
+  addIt(): void {
+    let myElement = this.element.nativeElement.querySelector('#header');
+    myElement.innerText = myElement.innerText + "!";
+  }
+
+  removeIt(): void {
+    let myElement = this.element.nativeElement.querySelector('#header');
+    myElement.innerText = myElement.innerText.substring(0, myElement.innerText.length - 1);
+  }
+}
